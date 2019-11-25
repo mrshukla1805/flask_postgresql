c56921ed55d6640780a6c335b2fd36ad4d49cf56
mrshukla <54001817+mrshukla1805@users.noreply.github.com>
2019-11-26 02:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask , request , render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aslongasyou@localhost/flask_books'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 
